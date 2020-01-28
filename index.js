b60202507c2f891654474387187124fd55a48425
manel turki <menelturki@gmail.com>
2020-01-28 15:27:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
@@ -13,4 +15,5 @@ function renderBooks(json) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+  getPosts()
 })
